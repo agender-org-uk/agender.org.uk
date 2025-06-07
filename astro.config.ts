1a16b0029719c848687e3fc547f05a3f7f50533a
Joe Roberts <joe@engiqueer.net>
2025-06-07 15:40:15 +0100

    Add preview environment

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   adapter: cloudflare(),
+  output: process.env.PREVIEW === "true" ? "server" : "static",
 });
